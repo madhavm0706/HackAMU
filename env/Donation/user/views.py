103ca454ae22b5a47f3f25f8ccf2589adf91a31c
Madhav Maheshwari <madhavm0706@gmail.com>
2020-04-11 15:55:10 +0530

    add templates and static files

--- env/Donation/user/views.py
+++ env/Donation/user/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request,'user/index.html',{})
+
+
+def UserLogin(request):
+    return render(request,'user/dlogin.html',{})
+
+def PoliceLogin(request):
+    return render(request,'user/plogin.html',{})    
